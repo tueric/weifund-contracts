802fbbe3f4f6672dd12460de9e80a2b59f1baf57
SilentCicero <silentcicero@outlook.com>
2016-07-13 20:17:48 -0400

    new standard token campaign design and interface

--- contracts/StandardTokenCampaign.sol
+++ contracts/StandardTokenCampaign.sol
@@ -4,31 +4,43 @@ import "Calculator.sol";
 import "Token.sol";
 
 contract StandardTokenCampaign is TokenCampaign, StandardCampaign {
-  function () {
-    throw;
-  }
-
-  function contributeMsgValue() {
-    contributionCreated[msg.sender] = now;
-    StandardCampaign.contributeMsgValue();
-  }
-
-  modifier senderContributionCreatedBeforeExpiry {
-    if(contributionCreated[msg.sender] < expiry) {
+  modifier beforeExpiry {
+    if(now < expiry) {
       _
     }
   }
 
-  function claimTokensOwed() reachedFundingGoal pastExpiry senderClaimNotMade senderContributionCreatedBeforeExpiry returns (address contributor, uint tokenAmountClaimed) {
+  function contributeMsgValue() beforeExpiry {
+    uint dispersalAmount = calculator.calculateAmount(msg.value, now);
+
+    if(token.balanceOf(address(this)) >= (dispersalAmount + totalTokensOwed)) {
+      totalTokensOwed += dispersalAmount;
+      tokensOwed[msg.sender] += dispersalAmount;
+      StandardCampaign.contributeMsgValue();
+    }
+  }
+
+  function claimTokensOwed() reachedFundingGoal pastExpiry senderClaimNotMade returns (address contributor, uint tokenAmountClaimed) {
     claimMade[msg.sender] = true;
     contributor = msg.sender;
-    tokenAmountClaimed = calculator.calculateAmount(contributionBalances[contributor], contributionCreated[contributor]);
+    if(msg.sender == owner) {
+      tokenAmountClaimed = token.balanceOf(address(this)) + tokensOwed[msg.sender] - totalTokensOwed - totalTokensDispersed;
+    } else {
+      tokenAmountClaimed = tokensOwed[msg.sender];
+    }
+    totalTokensDispersed += tokenAmountClaimed;
     if(token.transfer(contributor, tokenAmountClaimed)){
       TokensClaimed(contributor, tokenAmountClaimed);
     }
   }
 
-  mapping(address => uint) public contributionCreated;
-  Calculator calculator;
+  function tokensOwedTo(address _contributor) constant returns (uint256 amount) {
+    return tokensOwed[_contributor];
+  }
+
   Token token;
+  Calculator calculator;
+  mapping(address => uint) tokensOwed;
+  uint public totalTokensOwed;
+  uint public totalTokensDispersed;
 }
--- contracts/TokenCampaign.sol
+++ contracts/TokenCampaign.sol
@@ -2,10 +2,17 @@ import "Campaign.sol";
 
 contract TokenCampaign is Campaign {
 
-  /// @notice the latest time at which sender `_contributor` made a contribution
-  /// @param _contributor The contributor address
-  /// @return The latest time (specified as a uint256 unix timestamp) a contributor made a contribution
-  function contributionCreated(address _contributor) constant returns (uint256 created) {}
+  /// @notice the total amount of tokens owed to contributors
+  /// @return A uint256 value of the tokens owned to contributors
+  function totalTokensOwed() constant returns (uint256 amount) {}
+
+  /// @notice the net total amount of tokens dispersed from the campaign contract
+  /// @return A uint256 value of tokens dispersed
+  function totalTokensDispersed() constant returns (uint256 amount) {}
+
+  /// @notice the amount of tokens owed to the contributor
+  /// @return A uint256 value of the total amount of tokens owned to the contributor
+  function tokensOwnedTo(address _contributor) constant returns (uint256 amount) {}
 
   /// @notice get address of the dispersal calculator service contract used
   /// @return The dispersal calculator target address
