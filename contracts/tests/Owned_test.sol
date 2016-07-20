5f31beba622c037ea9c0f872686cce9d154352f8
SilentCicero <silentcicero@outlook.com>
2016-07-20 15:44:56 -0400

    test scaffolding for owned, claimproxy and standard campaign

--- contracts/tests/Owned_test.sol
+++ contracts/tests/Owned_test.sol
@@ -2,4 +2,9 @@ import "dapple/test.sol";
 import "Owned.sol";
 
 contract OwnedTests is Test {
+  function test_ownedProperties() {
+  }
+
+  function test_ownedOnlyOwnerModifier() {
+  }
 }
--- contracts/tests/StandardCampaign_test.sol
+++ contracts/tests/StandardCampaign_test.sol
@@ -2,4 +2,48 @@ import "dapple/test.sol";
 import "StandardCampaign.sol";
 
 contract StandardCampaignTests is Test {
+  function test_validProperties() {
+  }
+
+  function test_validConstruct() {
+  }
+
+  function testThrow_invalidFallbackViaSendContribution() {
+  }
+
+  function testThrow_invalidFallbackViaCallContirbution() {
+  }
+
+  function testThrow_invalidPayoutToBeneficiary() {
+  }
+
+  function test_validCampaignHasExpiredModifier() {
+  }
+
+  function testThrow_invalidCampaignHasExpiredModifier() {
+  }
+
+  function test_validCampaignHasNotExpiredModifier() {
+  }
+
+  function testThrow_invalidCampaignHasNotExpiredModifier() {
+  }
+
+  function test_campaignIsSuccessModifier() {
+  }
+
+  function testThrow_invalidCampaignIsSuccessModifier() {
+  }
+
+  function test_validBeneficiaryPayout() {
+  }
+
+  function testThrow_invalidBeneficiaryPayout() {
+  }
+
+  function test_validMultipleBeneficiaryPayout() {
+  }
+
+  function test_invalidMultipleBeneficiaryPayout() {
+  }
 }
