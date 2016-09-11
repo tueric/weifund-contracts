dcd1beb84d8d7e93ac98e4b8e13f08eb6b14b0e1
SilentCicero <silentcicero@outlook.com>
2016-09-11 18:47:55 +0200

    add standard campaign factory

--- contracts/Owner.sol
+++ contracts/Owner.sol
@@ -1,9 +1,4 @@
 contract Owner {
-  function Owner() {
-    // set the owner as the sender, if owner constructor used
-    owner = msg.sender;
-  }
-
   modifier onlyowner() {
     // only allow a message sender than is the owner
     if (msg.sender == owner) {
